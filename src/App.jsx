81e188b7bcc6dea3068d89850f61592de91738e2
AkashMarkile-1947 <akashmarkile00@gmail.com>
2022-11-05 10:37:37 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import discordLogo from "../assets/discord_main_logo.svg";
+import Hero from "./landingpage/hero";
+import Navbar from "./landingpage/navbar";
+function App() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
+
+export default App;
---
+++ src/landingpage/inviteonly.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+import inviteOnly from '../../assets/invite_only_landing.svg';
+
+const inviteOnly = () => {
+
+}
+
+
+export default inviteOnly;
